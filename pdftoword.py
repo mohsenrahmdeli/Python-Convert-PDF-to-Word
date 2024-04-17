54b49bd755b52aade29b8b0046dde7a1692bd3ec
mohsenrahmdeli <mohsenrahmdeli@gmail.com>
2024-04-17 09:05:13 +0400

    Initialize Repository

---
+++ pdftoword.py
@@ -0,0 +1,12 @@
+import aspose.words as aw
+import os
+
+
+pdf_path = r"You Path of PDF File"
+doc = aw.Document(pdf_path)
+
+# Separate filename and extension
+file_name, file_extension = os.path.splitext(pdf_path)
+
+# Save the Word file with the same name as the PDF file
+doc.save(file_name + ".docx")
